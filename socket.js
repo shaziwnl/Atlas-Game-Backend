07013124fd4aa58775f523d0fad56ac36da5f5c0
Sharang Goel <goelshaz001@gmail.com>
2024-02-06 20:03:47 -0500

    fixed URL

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://atlas-game.netlify.app',
     methods: ['GET', 'POST']
   }
 });
